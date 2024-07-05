b4cc07edcd1212a15f48b19ddc6a5d7dec799e5c
itz-marcus <marcusadonis5@gmail.com>
2024-07-05 08:04:05 +0200

    updated Testimonials

--- src/views/TestimonialsView.vue
+++ src/views/TestimonialsView.vue
@@ -1,5 +1,6 @@
 <template lang="">
     <div>
+        <img src="https://itz-marcus.github.io/Project_images/5th_page_clear.png" alt="backgrounds2" loading="lazy">
         <div class="card" style=" margin-left:0.7%;margin-top: 3.5%; width: 17rem; height: 32rem;background-color: rgba(0, 0, 0, 0.708);color: white;box-shadow:0px 1px 1px 4px  rgb(255, 230, 0);" >
                 <img id="card-cover" src="https://itz-marcus.github.io/Project_images/M-B.png" class="card-img-top" alt="...">
                 <div id="proj-b" class="card-body">
@@ -16,7 +17,19 @@ export default {
 }
 </script>
 <style>
- .card{
+img[alt='backgrounds2']{
+    height: 75vh;
+    top:10%;
+    background-repeat: no-repeat;
+    opacity: 0.5; 
+    position: absolute;
+    z-index: -1;
+    left: 15%;
+    margin-left: auto;
+    margin-right:auto;
+    object-fit: contain;
+}
+.card{
     font-size: 20px;
 }
 #card-cover{
